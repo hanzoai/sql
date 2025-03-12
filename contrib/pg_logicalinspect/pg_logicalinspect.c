hanzoai/sql

commit 02bc8959a5159874c1324f9e243a78f585dda063

Fix compiler warning in pg_logicalinspect.
Oversight in e2d06f6a108.

Reported-by: David Rowley <dgrowleyml@gmail.com>
Reported-by: Nathan Bossart <nathandbossart@gmail.com>
Author: David Rowley <dgrowleyml@gmail.com>
Discussion: https://postgr.es/m/CAApHDvqrhFfnetbcwgGkJ=z63T8HfQ_OyP=vX8BYiXyxFKt67w@mail.gmail.com

contrib/pg_logicalinspect/pg_logicalinspect.c
@@ -85,6 +85,8 @@ parse_snapshot_filename(const char *filename)
 parse_error:
 	ereport(ERROR,
 			errmsg("invalid snapshot file name \"%s\"", filename));
+
+	return InvalidXLogRecPtr;					/* keep compiler quiet */
 }
 
 /*

commit 5c1010da21a20b193ab50741df16a879d5a25134

Fix indentation issue
Introduced recently by 02bc8959a

Per buildfarm member koel

contrib/pg_logicalinspect/pg_logicalinspect.c
@@ -86,7 +86,7 @@ parse_error:
 	ereport(ERROR,
 			errmsg("invalid snapshot file name \"%s\"", filename));
 
-	return InvalidXLogRecPtr;					/* keep compiler quiet */
+	return InvalidXLogRecPtr;	/* keep compiler quiet */
 }
 
 /*